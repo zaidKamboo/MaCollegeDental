f925e766e7e21e1bfdd2551607fd7590a17e3f9f
zaid <zaidkamboo100@gmail.com>
2023-09-14 16:55:39 +0530

    Done with Alumini Association Committee

--- src/Components/Students/Alumini/AluminiAssociationCommittee/AluminiAssociationCommittee.jsx
+++ src/Components/Students/Alumini/AluminiAssociationCommittee/AluminiAssociationCommittee.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import "./AluminiAssociationCommittee.css";
+import bki from "../../../../Images/2k8b.jpg"
 const AluminiAssociationCommittee = () => {
     return (
-        <div>
-            Alumini Association Committee.
+        <div className='aacContainer'>
+            <h2 className="headinga">
+                ALUMINI
+            </h2>
+            <center><h3 className='hed'> CONTRIBUTION OF “ALUMNI ASSOCIATION OF M.C.E SOCIETY’S
+                M. A. RANGOONWALA DENTAL COLLEGE” IN LAST 5 YEARS
+            </h3></center>
+            <p className="content">
+                The Alumni association of MARDC was registered in December 2012 under the aegis of MCE SOCIETY. Shri Dr. P.A Inamdar, President MCE Society or a duly appointed member by him on his behalf were President of the Alumni Association. Dr. Ramandeep Dugal, Principal MARDC, few staff members and students from the council were among the registered body members of the association. Since then regular meetings of alumni at different occasions have taken place. A recurring ALUMNI FEST-“RECALIA” was conceptualized in 2013  and is organized by Alumni association MARDC every alternate year. This meet is a blend of academic and cultural activities including music, talent show, dramatics, current affairs, carnival, banquet etc. At RECALIA which means Recall, we avidly propagate our motto MEET, CONNECT & IMBIBE. We aimed at creating a platform for ex- students to come back to their alma mater, meet and reconnect with old and new, exchange ideas, and share some memorable moments and Imbibe or absorb good values, great ideas, assimilate good thoughts and knowledge. We also introduced the ALUMNI FORUM where some of the alumni, who have undergone specialized training or achieved extraordinary skills were invited for pedogogy and didactic presentations. The association has elected office bearers including President, Vice –President, Secretary, Joint Secretary, Treasurer, Members (Patron, Life, Affiliate, Donor, Student, Honorary) etc. The core committee along with Alumni members in the college host the meet with innovative ideas and current trends and welcome their colleagues and seniors from different places in India and abroad. The participants of these meetings willingly fill up feedback forms to share their experiences and give valuable suggestions for the betterment and enrichment of the institution, association, clinical and research material and the overall development and well being of the student fraternity. The 2000 strong MARDC Alumni association also has a presence on social media with networking of several members across the globe and virtual alumni meets as part of the new normal. MARDC being a post graduate institution also has micro alumni meets at the individual department level, for their post graduate alumni, who are invited for guest lectures and interactive sessions. Donations and contributions such as books, equipment, study material, articles, library data sources, training for entrance exam aspirants etc are some of the highlights of these interactions. Regular meetings annually or at different frequencies, depending on the feasibility include- Cluster meetings at a common place, city or international destination, batch-wise meetings, department wise meetings of the postgraduate alumni. The Annual college magazine published each year also generally features an alumni column or a corner wherein some of them share their experiences, or are interviewed as guests to felicitate or laud their achievements in the profession. Sport events being an integral part of student life are always cherished by all. Every year during annual cultural week, a sports event including the much awaited cricket match between alumni and the students is held. An overwhelming response from the ex students who are more than willing to return back to their alma mater is the basis and strength of this association.
+            </p>
+            <img className='i' src={bki} alt="" />
+            <center><b className='vc'>
+                Batch-2008 Visiting College</b></center>
         </div>
     )
 }
